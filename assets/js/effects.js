cd002ff843a6bcb2154504c54186465ab74117e7
Arno <arno.arzumanyan@student.howest.Be>
2018-06-28 00:44:04 +0200

    Added some effects scroll down is not yet so good!!

---
+++ assets/js/effects.js
@@ -0,0 +1,11 @@
+document.addEventListener("mousewheel", fadeIn);
+
+
+
+function fadeIn(e){
+
+    document.getElementById("about").classList.add("fadeInLeft");
+    document.getElementById("about").classList.add("animated");
+
+
+}
