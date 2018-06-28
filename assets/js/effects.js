e1b3d13ed4427cbd977128fe057cd0cacf0d7303
Arno <arno.arzumanyan@student.howest.Be>
2018-06-28 23:54:10 +0200

    Added some new animation delays and effects

--- assets/js/effects.js
+++ assets/js/effects.js
@@ -1,4 +1,24 @@
-document.addEventListener("mousewheel", fadeIn);
+"use strict";
+
+document.addEventListener('DOMContentLoaded', init);
+
+
+function init() {
+
+    document.addEventListener("mousewheel", fadeIn);
+
+
+    for (let i = 0; i < document.querySelectorAll("#songs iframe").length; i++){
+
+        document.querySelectorAll("#songs iframe")[i].addEventListener("mouseover",stopAnimation);
+        document.querySelectorAll("#songs iframe")[i].addEventListener("click",stopAnimation);
+
+    }
+
+
+}
+
+
 
 
 
@@ -6,6 +26,17 @@ function fadeIn(e){
 
     document.getElementById("about").classList.add("fadeInLeft");
     document.getElementById("about").classList.add("animated");
+
+
+}
+
+
+
+function stopAnimation(e) {
+
+    e.target.classList.toggle("pulse");
+    e.target.classList.toggle("animated");
+    e.target.classList.toggle("infinite");
 
 
 }
